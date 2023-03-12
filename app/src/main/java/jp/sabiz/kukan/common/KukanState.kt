7a712fa7cbfa557eda11d1cbade746c9967494ad
sAbIz <bellett@sabiz.jp>
2023-03-12 09:27:05 +0900

    DBのデータをエクスポート可能にする
    DB内のデータをCSV形式にShare intentの発行を行う
    intentの結果がCANCELではない場合DBのデータを削除可能にする

--- app/src/main/java/jp/sabiz/kukan/common/KukanState.kt
+++ app/src/main/java/jp/sabiz/kukan/common/KukanState.kt
@@ -2,7 +2,6 @@ package jp.sabiz.kukan.common
 
 enum class KukanState {
     OFF,
-    PAUSE,
     ON;
 
     fun toOn(): KukanState {
@@ -17,7 +16,6 @@ enum class KukanState {
         return when(this){
             ON -> OFF
             OFF -> ON
-            else -> this
         }
     }
 }
